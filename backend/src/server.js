bf66b6e7598eab7676dc74eaca237bcf2700761a
Anup Rathore <anuprathore1669@gmail.com>
2025-10-25 11:37:08 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,22 @@ import connectDB  from './config/db.js';
 import dotenv from 'dotenv';
 import FirstRateLimiter from './middlewares/FirstRateLimiter.js';
 import cors from 'cors';
+import path from 'path';
+
+const __dirname = path.resolve();
+
+
 
 dotenv.config();
 const app = express();
-app.use(express.json()); // this is the middleware to parse json data
+app.use(express.json()); 
+// this is the middleware to parse json data
+
+if (process.env.NODE_ENV !== "production") {
 app.use(cors({
     origin: "http://localhost:5173", // Replace with your frontend URL
 }))
+}
 app.use(FirstRateLimiter);
 
 
@@ -18,8 +27,16 @@ app.use(FirstRateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname , "../frontend/dist")));
+
+app.get ("*" , (req,res)=>{
+    res.sendFile(path.join(__dirname , "../frontend" ,"dist" ,"index.html"));
+    })
+}
 
 
+const PORT = process.env.PORT || 5001;
 connectDB().then(()=>{
     app.listen(process.env.PORT, ()=>{
     console.log(`Server is running on ${process.env.PORT}`);
